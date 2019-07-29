6431e6f5364f09453bffa9bf17d077a1151c66ad
Daniel Bergey <bergey@teallabs.org>
2019-07-29 20:10:35 +0000

    use FontyFruity to find fonts by OS

--- src/Graphics/Gudni/Interface/FontLibrary.hs
+++ src/Graphics/Gudni/Interface/FontLibrary.hs
@@ -1,6 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE CPP #-}
 
------------------------------------------------------------------------------
 -- |
 -- Module      :  Graphics.Gudni.Interface.FontLibrary
 -- Copyright   :  (c) Ian Bloom 2019
@@ -26,10 +26,17 @@ import System.FilePath (addTrailingPathSeparator, normalise)
 import System.Directory (getHomeDirectory, getDirectoryContents)
 import Data.Maybe (fromJust, fromMaybe, listToMaybe)
 import Data.List (isPrefixOf)
+import Data.Monoid (First(..))
+import Graphics.Text.TrueType (buildCache, enumerateFonts, findFontInCache, FontDescriptor(..), FontCache(..), FontStyle(..))
 
 
 findDefaultFont :: IO String
-findDefaultFont = fromMaybe "Times New Roman.ttf" <$> listToMaybe . filter (isInfixOf "Times New Roman.ttf") <$> fontLibrary
+findDefaultFont = do
+    cache <- buildCache
+    -- Try several families in order of preference
+    let roman family = FontDescriptor family (FontStyle False False)
+    return $ fromMaybe "Times New Roman.ttf" $ getFirst $ foldMap First $
+        [findFontInCache cache (roman family) | family <- [ "Times New Roman", "Liberation Serif" ] ]
 
 -- | Make a relative path absolute on MacOS.
 absolutizeMacPath :: String -> IO String
@@ -53,7 +60,6 @@ absoluteDirectoryContents dir =
 
 -- | Return a list of loadable font files on the system.
 fontLibrary :: IO [String]
-fontLibrary =
-  do dirs <- fontDirectories
-     files <- concat <$> mapM absoluteDirectoryContents dirs
-     return $ filter (isSuffixOf "ttf") files
+fontLibrary = do
+    cache <- buildCache
+    return $ map (fromJust . findFontInCache cache) (enumerateFonts cache)
