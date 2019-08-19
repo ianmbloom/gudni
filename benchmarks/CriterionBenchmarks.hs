4ddc12de2e631b14e40be7c872b325826705a207
Daniel Bergey <bergey@teallabs.org>
2019-08-19 15:19:25 +0000

    benchmarks for CanProject

--- benchmarks/CriterionBenchmarks.hs
+++ benchmarks/CriterionBenchmarks.hs
@@ -1,16 +1,17 @@
 {-# LANGUAGE TypeApplications #-}
 module CriterionBenchmarks where
 
-import Graphics.Gudni.Figure.OpenCurve
-import Graphics.Gudni.Figure.Segment
+import           Graphics.Gudni.Figure.OpenCurve
+import           Graphics.Gudni.Figure.Segment
+import           Graphics.Gudni.Figure.Space
 
 import qualified Graphics.Gudni.Figure.Bezier as BZ
 
-import Linear
-import Linear.Affine
+import           Linear hiding (project)
+import           Linear.Affine
 
-import Criterion
-import Criterion.Main
+import           Criterion
+import           Criterion.Main
 
 main = defaultMain
     [ bench "length of single Bézier segment, analytical" $
@@ -29,10 +30,29 @@ main = defaultMain
                          , Seg (P (V2 1 0)) (Just (P (V2 1.5 (-0.5)))) ]
                          (P (V2 2 0)))
     , bench "inverseArcLength, Float, ε = 1e-6" $
-        nf (BZ.inverseArcLength @Float 1e-6 bezier0) 0.5
+        let ε = 1e-6
+        in nf (BZ.inverseArcLength @Float (BZ.maxStepsFromAccuracy ε) (Just ε)  bezier0) 0.5
     , bench "inverseArcLength, Float, ε = 1e-9" $
-        nf (BZ.inverseArcLength @Float 1e-9 bezier0) 0.5
+        let ε = 1e-9
+        in nf (BZ.inverseArcLength @Float (BZ.maxStepsFromAccuracy ε) (Just ε)  bezier0) 0.5
+    -- , bench "projectPoint, Float, ε = 1e-6" $
+    --     let ε = 1e-6
+    --     in nf (projectPoint (BZ.maxStepsFromAccuracy ε) (Just ε)  curve0) curve0
+    , bench "projectWithStepsAccuracy, Float, ε = 1e-6" $
+        let ε = 1e-6
+        in nf (projectWithStepsAccuracy (BZ.maxStepsFromAccuracy ε) (Just ε) curve0) curve0
+    , bench "projectWithAccuracy, Float, ε = 1e-6" $
+      nf (projectWithAccuracy 1e-6  curve0) curve0
+    , bench "projectWithSteps, Float" $
+      nf (projectWithSteps (BZ.maxStepsFromAccuracy 1e-6) curve0) curve0
+    , bench "project, Float" $
+      nf (project curve0) curve0
     ]
 
 bezier0 :: Fractional s => BZ.Bezier s
-bezier0 = V3 (P (V2 0 0)) (P (V2 0.5 0.5)) (P (V2 1 0))
+bezier0 = V3 (P (V2 0 0)) (P (V2 0.75 0.5)) (P (V2 1 0))
+
+curve0 :: OpenCurve Float
+curve0 = OpenCurve
+    [Seg (P (V2 0 0)) (Just (P (V2 0.75 0.5))), Seg (P (V2 1 0)) (Just (P (V2 1.25 (-0.75))))]
+    (P (V2 2 0))
